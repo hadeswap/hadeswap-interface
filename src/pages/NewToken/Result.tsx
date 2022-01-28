31ab8c9607c91534dc3073f4a7d606c0c18a0d1e
Niriot-dotcom <artemis@polispay.org>
2022-01-28 00:06:39 -0600

    location.state from button updated.

--- src/pages/NewToken/Result.tsx
+++ src/pages/NewToken/Result.tsx
@@ -42,7 +42,10 @@ export default function Result(props: FuncProps) {
             <div style={{padding:'25px',display: 'flex',  justifyContent:'center', alignItems:'center'}}>
                 <ButtonPrimary style={{width:'30rem'}}>
                     <NavLink 
-                        to={'/newsale'}>
+                        to={{
+                            state: { data: ':D' },
+                            pathname: '/newsale',
+                        }}>
                         {i18n._(t`New Sale`)}
                     </NavLink>
                 </ButtonPrimary>
