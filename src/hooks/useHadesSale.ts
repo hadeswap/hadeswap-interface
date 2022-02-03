dc32d7e7fa440d8900622b67e94be0aafd3e19f5
sara_carolina12 <saracarogode@gmail.com>
2022-02-03 11:04:32 -0600

    'smart-contract-new-sale'

--- src/hooks/useHadesSale.ts
+++ src/hooks/useHadesSale.ts
@@ -13,14 +13,14 @@ import { CROWDSALE_TEMPLATE } from '../constants'
 // const useHadesSale = (tokenAddress: string) => {
 const useHadesSale = () => {
     // const [approval, approveCallback] = useApproveCallback(independentAmount, TOKEN_FACTORY_ADDRESS)
+    // let tokenAddress = sara 
+    // let tknTx = '0x2cf3d53581fb657d85a6b490ab47a3aee0506ed52859daa3df18bd5bdb499a29' // token
+
     const sara = '0x0AF4366C25c49C46b4816c42fEe25a0cf67dF2aB' // wallet
-    let tokenAddress = sara 
-    let tknTx = '0x2cf3d53581fb657d85a6b490ab47a3aee0506ed52859daa3df18bd5bdb499a29'
+    const tokenAddress = '0x2cf3d53581fb657d85a6b490ab47a3aee0506ed52859daa3df18bd5bdb499a29' // token
     
-    const tokenFactoryContract = useTokenFactoryContract()
     const tokenContract = useTokenContract(tokenAddress)
     const auctionFactoryContract = useAuctionFactoryContract()
-    const {account} = useActiveWeb3React()
     const crowdsaleContract = useCrowdsaleContract();
 
 
@@ -35,18 +35,25 @@ const useHadesSale = () => {
             let timestamp5 = Math.floor(Date.now() / 1000) + 1000;
 
             try {
+                // step 2
                 await tokenContract?.approve(auctionFactoryContract?.address, '10000000000000000000000000', {from: sara});
+
+                // step 3
+                let tid = await auctionFactoryContract?.getTemplateId(crowdsaleContract?.address);
+                console.log("try 3");
                 let data = await crowdsaleContract?.getCrowdsaleInitData(auctionFactoryContract?.address,
-                                 tokenAddress, '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE', '1000000000000000000000',
+                                 tokenContract?.address, '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE', '1000000000000000000000',
                                  timestamp, timestamp5, '10000000000000000', '1000000000000000000',
                                  sara, zero_address, sara);
-                await tokenContract?.approve(auctionFactoryContract?.address, '1000000000000000000', {from: sara})
-                
+                console.log("data: ", data);                
                 console.log("try 4");
-                let tx = await auctionFactoryContract?.createMarket(tknTx, '1000000000000000000000', zero_address, data, {from: sara});
+                await tokenContract?.approve(auctionFactoryContract?.address, '1000000000000000000', {from: sara})
+
+                let tx = await auctionFactoryContract?.createMarket(tid, tokenAddress, '1000000000000000000000', zero_address, data, {from: sara});
                 console.log("try 5");
-                let crowdsaleAddress = tx.logs[0].args.addr; //create token, create market crea 
-                console.log('crowdsaleAddress', crowdsaleAddress)
+                
+                let crowdsaleAddress = tx.logs[0].args.addr; // create token, create market crea 
+                console.log('crowdsaleAddress: ', crowdsaleAddress)
 
                 const tx_data = await tx.wait()
                 console.log('tx', tx_data)
@@ -56,7 +63,7 @@ const useHadesSale = () => {
                 return undefined
             }
         },
-        [tokenFactoryContract]
+        []
     )
 
     return { createSale }
