97dc76246a287e9ba19be7a440fa95609ba7b3f2
Niriot-dotcom <artemis@polispay.org>
2022-02-10 08:06:06 -0600

    WIP useHadesSale hook.

--- src/hooks/useHadesSale.ts
+++ src/hooks/useHadesSale.ts
@@ -6,17 +6,17 @@ import {
     useCrowdsaleContract
 } from 'hooks/useContract'
 import React, { useCallback, useEffect, useState } from 'react'
-import { useTransactionAdder } from '../state/transactions/hooks'
 import useActiveWeb3React from './useActiveWeb3React'
-import { CROWDSALE_TEMPLATE } from '../constants'
 
 const useHadesSale = () => {
-    const sara = '0xDB8fD34B637a14c8DacF9ECfF51a6A7E5387B720' // wallet
-    const tokenAddress = '0x74Eb00a3631096d2DBC4a9bE1f6A24B89cFF6340' // token
+    // const sara = '0xDB8fD34B637a14c8DacF9ECfF51a6A7E5387B720' // wallet
+    // const tokenAddress = '0x74Eb00a3631096d2DBC4a9bE1f6A24B89cFF6340' // token
+    const [tokenFeeCost, setTokenFeeCost] = useState('0');
     
     const tokenContract = useTokenContract(tokenAddress)
     const soulContract = useTokenContract('0xf1498e8103359fD96c5E08fb34b4C249B619025a')
     const auctionFactoryContract = useAuctionFactoryContract()
+    const {account} = useActiveWeb3React()
     const crowdsaleContract = useCrowdsaleContract();
 
     const zero_address = '0x0000000000000000000000000000000000000000';
@@ -24,28 +24,13 @@ const useHadesSale = () => {
 
     // Create Sale
     const createSale = useCallback(
-        async () => {
+        async (tid: string , data: string, name: string, symbol: string) => {
             console.log('creating sale...')
-            let timestamp = Math.floor(Date.now() / 1000) + 100;
-            let timestamp5 = Math.floor(Date.now() / 1000) + 1000;
 
             try {
-                // step 2
-                await tokenContract?.approve(auctionFactoryContract?.address, '10000000000000000000000000', {from: sara});
-
-                // step 3
-                let tid = await auctionFactoryContract?.getTemplateId(crowdsaleContract?.address);
-                let data = await crowdsaleContract?.getCrowdsaleInitData(auctionFactoryContract?.address,
-                                 tokenContract?.address, '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE', '1000000000000000000000',
-                                 timestamp, timestamp5, '10000000000000000', '1000000000000000000',
-                                 sara, zero_address, sara);
-                console.log("data: ", data);                
-                await soulContract?.approve(auctionFactoryContract?.address, '10000000000000000000000000', {from: sara})
-                console.log("try 4");
-                let tx = await auctionFactoryContract?.createMarket(tid, tokenAddress, '1000000000000000000000', zero_address, data, {from: sara});
-                console.log("try 5");
-                let crowdsaleAddress = tx.logs[0].args.addr; // create token, create market crea 
-                console.log('crowdsaleAddress: ', crowdsaleAddress)
+                const tx = await auctionFactoryContract?.createMarket(tid, tokenAddress, '1000000000000000000000', zero_address, data, {from: sara});
+                // let crowdsaleAddress = tx.logs[0].args.addr;
+                // console.log('crowdsaleAddress: ', crowdsaleAddress)
 
                 const tx_data = await tx.wait()
                 console.log('tx', tx_data)
@@ -58,7 +43,48 @@ const useHadesSale = () => {
         []
     )
 
-    return { createSale }
+    const getCrowdsaleData = async (
+        startTime: string | undefined,
+        endTime: string | null | undefined,
+        price: string | undefined,
+        goal: string | undefined,
+        admin: string | undefined,
+        wallet: string | undefined,
+    ): Promise<string> => {
+        try {
+            // const iSupply = ethers.utils.parseUnits(supply??'0', 18)
+            // let iOwner: any = owner;
+            // if(owner === '') {
+            //     iOwner = account
+            // }
+            const data = await crowdsaleContract?.getCrowdsaleInitData(auctionFactoryContract?.address,
+                tokenContract?.address, '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE', '1000000000000000000000',
+                startTime, endTime, price, goal,
+                admin, zero_address, wallet);
+            console.log("data: ", data);
+            return data
+            // return await tokenContract?.getInitData(name, symbol, iOwner, iSupply)
+        } catch (e) {
+            return zero_data
+        }
+    }
+
+    const getTokenFeeCost = async () => {
+        try {
+            const fee =  await tokenFactoryContract?.minimumFee()
+            setTokenFeeCost(ethers.utils.formatUnits(fee, 18))
+        } catch (e) {
+            console.log(e)
+            setTokenFeeCost('0')
+        }
+    }
+
+    useEffect(() => {
+        getTokenFeeCost()
+
+    }, [account, tokenFactoryContract])
+
+    return { createSale, getCrowdsaleData, getTokenFeeCost }
 }
 
 export default useHadesSale
