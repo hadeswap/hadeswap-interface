665f7b64db481b3da58d764c4b15de225fb49c31
sara_carolina12 <saracarogode@gmail.com>
2022-02-11 22:40:53 -0600

    'fixed-error'

--- src/hooks/useHadesSale.ts
+++ src/hooks/useHadesSale.ts
@@ -11,6 +11,7 @@ import useActiveWeb3React from './useActiveWeb3React'
 const useHadesSale = () => {
     const auctionFactoryContract = useAuctionFactoryContract()
     const crowdsaleContract = useCrowdsaleContract();
+    const soulContract = useTokenContract('0xf1498e8103359fD96c5E08fb34b4C249B619025a')
     const { account } = useActiveWeb3React()
 
     const zero_address = '0x0000000000000000000000000000000000000000';
@@ -19,31 +20,67 @@ const useHadesSale = () => {
     const createSale = useCallback(
         async (tid: string, data: string, tokenAddress: string, owner: string | undefined, supply: string | undefined) => {
             console.log('creating sale...')
+
             try {
-                const iSupply = ethers.utils.parseUnits(supply??'0', 18);
-                let iOwner: any = owner;
-                if(owner === '') {
-                    iOwner = account
-                }
+                // step 2
+                // await crowdsaleContract?.approve(auctionFactoryContract?.address, '10000000000000000000000000', {from: owner});
+                const startTime = Math.floor(Date.now() / 1000) + 100;
+                const endTime = Math.floor(Date.now() / 1000) + 1000;
+                // step 3
+                let tid = await auctionFactoryContract?.getTemplateId(crowdsaleContract?.address);
+                let data = await crowdsaleContract?.getCrowdsaleInitData(auctionFactoryContract?.address,
+                                 tokenAddress, '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE', '1000000000000000000000',
+                                 startTime, endTime, '10000000000000000', '1000000000000000000',
+                                 owner, zero_address, owner);
+                console.log("data: ", data);
+                // await soulContract?.approve(auctionFactoryContract?.address, '10000000000000000000000000', {from: owner})
+                console.log("try 4");
+
+                
                 console.log('creating sale params...')
                 console.log('tid: ', tid);
                 console.log('data: ', data);
                 console.log('tokenAddress: ', tokenAddress);
-                console.log('iOwner: ', iOwner);
-                console.log('iSupply: ', iSupply);
-                
-                const tx = await auctionFactoryContract?.createMarket(tid, tokenAddress, iSupply, zero_address, data, {from: iOwner});
-                // let crowdsaleAddress = tx.logs[0].args.addr;
-                // console.log('crowdsaleAddress: ', crowdsaleAddress)
+                console.log('iOwner: ', owner);
+                console.log('iSupply: ', supply);
+
+                let tx = await auctionFactoryContract?.createMarket(tid, tokenAddress, '1000000000000000000000', zero_address, data, {from: owner});
+                console.log("try 5");
+                let crowdsaleAddress = tx.logs[0].args.addr; // create token, create market crea 
+                console.log('crowdsaleAddress: ', crowdsaleAddress)
 
                 const tx_data = await tx.wait()
-                console.log('crowdsale tx: ', tx_data)
+                console.log('tx', tx_data)
                 return tx_data
-                return undefined
             } catch (e) {
                 console.error("createSale::useCallback error: ", e)
                 return undefined
             }
+            // try {
+            //     const iSupply = ethers.utils.parseUnits(supply??'0', 18);
+            //     let iOwner: any = owner;
+            //     if(owner === '') {
+            //         iOwner = account
+            //     }
+            //     console.log('creating sale params...')
+            //     console.log('tid: ', tid);
+            //     console.log('data: ', data);
+            //     console.log('tokenAddress: ', tokenAddress);
+            //     console.log('iOwner: ', iOwner);
+            //     console.log('iSupply: ', iSupply);
+                
+            //     const tx = await auctionFactoryContract?.createMarket(tid, tokenAddress, iSupply, zero_address, data, {from: iOwner});
+            //     // let crowdsaleAddress = tx.logs[0].args.addr;
+            //     // console.log('crowdsaleAddress: ', crowdsaleAddress)
+
+            //     const tx_data = await tx.wait()
+            //     console.log('crowdsale tx: ', tx_data)
+            //     return tx_data
+            //     return undefined
+            // } catch (e) {
+            //     console.error("createSale::useCallback error: ", e)
+            //     return undefined
+            // }
         },
         []
     )
@@ -60,7 +97,7 @@ const useHadesSale = () => {
         try {
             const startTime = Date.parse(startDate)/1000;
             const endTime = Date.parse(endDate)/1000;
-            const iSupply = ethers.utils.parseUnits(supply??'0', 18)
+            const iSupply = ethers.utils.parseUnits(supply??'0', 18) 
             const iPrice = ethers.utils.parseUnits(price??'0', 18);
             const iGoal = ethers.utils.parseUnits(goal??'0', 18);
             let iOwner: any = owner;
