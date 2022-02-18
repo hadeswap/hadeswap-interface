bc6e9e80a77f2eb1fb3044f3be08f86a684ff899
Niriot-dotcom <artemis@polispay.org>
2022-02-17 22:37:06 -0600

    init createLauncher hook.

--- src/hooks/useHadesLauncher.ts
+++ src/hooks/useHadesLauncher.ts
@@ -71,6 +71,55 @@ const useHadesLauncher = () => {
         }
     }
 
+    const createLauncher = useCallback(
+        async (owner: string | undefined, tokenAddress: string, crowdsaleAddress: string) => {
+        // async (tid: string , data: string, name: string, symbol: string) => {
+            // console.log('creating...', tid, data, name, symbol)
+            try {
+                // STEP: Approve to liquidityFactory
+                await token.approve(liquidityFactoryContract?.address, '10000000000000000000000000', {from: owner});
+                
+                // console.log((await token.balance).toString());
+
+                // STEP: Create Launcher
+                // get template id
+                const tid = await liquidityFactoryContract?.getTemplateId(postauctionlauncher.address);
+                console.log("launcher template id: ", tid.toString());
+                // get init data
+                
+                // create
+                const tx = await liquidityFactoryContract?.createLauncher(tid, tokenAddress, '1000000000000000000000', zero_address, data, {from: owner});
+                let launcherAddress = tx.logs[0].args.addr;
+                console.log('2', (await token.balanceOf(divi)).toString() )
+
+                const tx_data = await tx.wait()
+                console.log('tx', tx_data)
+                return tx_data
+            } catch (e) {
+                console.error('createLauncher::ERROR: ', e)
+                return undefined
+            }
+        },
+        []
+    )
+
+    const getLauncherData = async (
+        owner: string | null | undefined,
+        crowdsaleAddress: string,
+    ): Promise<string> => {
+        try {
+            let iOwner: any = owner;
+            if(owner === '') {
+                iOwner = account
+            }
+            const data = await postauctionlauncher.getLauncherInitData(crowdsaleAddress, factory.address, iOwner, iOwner, 900, 1000);
+            // await token.approve(liquidityFactoryContract, '1000000000000000000', {from: iOwner})
+            return data
+        } catch (e) {
+            return zero_data
+        }
+    }
+
     useEffect(() => {
         getTokenFeeCost()
 
